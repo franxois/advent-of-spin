360baa96716e0a88adb9eaa09596bbf88e4dcfae
Franxois <franxoois+github@gmail.com>
2023-12-07 12:34:05 +0100

    feat: add first version of challenge 2

---
+++ 2023/Challenge-2/server/src/lib.rs
@@ -0,0 +1,85 @@
+use serde::{Deserialize, Serialize};
+use spin_sdk::http::{Method, Request, Response};
+use spin_sdk::http_component;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Input {
+    pub kids: Vec<u8>,
+    pub weight: Vec<u8>,
+    pub capacity: u8,
+}
+
+#[derive(Debug)]
+pub struct Home {
+    pub kids: u8,
+    pub weight: u8,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Output {
+    pub kids: u8,
+}
+
+/// A simple Spin HTTP component.
+#[http_component]
+async fn handle_server(req: Request) -> anyhow::Result<Response> {
+    println!("Handling request to {:?}", req.header("spin-full-url"));
+
+    if *req.method() != Method::Post {
+        return Ok(Response::builder()
+            .status(200)
+            .header("content-type", "text/plain")
+            .body("Method not allowed")
+            .build());
+    }
+
+    let body: Vec<u8> = req.into_body();
+
+    let input: Input = serde_json::from_slice(body.as_slice())?;
+
+    println!("Input: {:?}", input);
+
+    let mut homes = input
+        .kids
+        .iter()
+        .enumerate()
+        .map(|(i, x)| {
+            return Home {
+                kids: *x,
+                weight: input.weight[i],
+            };
+        })
+        .collect::<Vec<_>>();
+
+    homes.sort_by(|a, b| {
+        let a_ratio = a.weight / a.kids;
+        let b_ratio = b.weight / b.kids;
+
+        if a_ratio < b_ratio {
+            return std::cmp::Ordering::Less;
+        } else if a_ratio > b_ratio {
+            return std::cmp::Ordering::Greater;
+        } else {
+            return std::cmp::Ordering::Equal;
+        }
+    });
+
+    let mut kid_numbers = 0;
+    let mut capacity = input.capacity;
+
+    homes.iter().for_each(|h| {
+        if capacity >= h.weight {
+            capacity -= h.weight;
+            kid_numbers += h.kids;
+        }
+    });
+
+    let result = Output { kids: kid_numbers };
+    let r = serde_json::to_string(&result)?;
+
+    Ok(Response::builder()
+        .status(200)
+        .header("content-type", "application/json")
+        .body(format!("{}", r))
+        .build())
+}
